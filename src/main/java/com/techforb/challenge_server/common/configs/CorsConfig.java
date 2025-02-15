98d5f3bde748c53e1fe4d9f351f35c6e55530614
Tobias Moreno <tobiasmoreno.tm.21@gmail.com>
2025-02-15 14:09:11 -0300

    fix: url for CORS in production

--- src/main/java/com/techforb/challenge_server/common/configs/CorsConfig.java
+++ src/main/java/com/techforb/challenge_server/common/configs/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200", "https://global-monitoring-tobias-moreno-dev.netlify.app/api")
+						.allowedOrigins("http://localhost:4200", "https://global-monitoring-tobias-moreno-dev.netlify.app/auth")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
